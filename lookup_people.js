e511ac0f5870ca161e358a18b32299c14b3929ea
S-Marks <marks.cherie.art@gmail.com>
2019-06-18 20:33:21 +0000

    read first and last name from command line

--- lookup_people.js
+++ lookup_people.js
@@ -2,23 +2,45 @@ const pg = require("pg");
 const settings = require("./settings"); // settings.json
 
 const client = new pg.Client({
-  user     : settings.user,
-  password : settings.password,
-  database : settings.database,
-  host     : settings.hostname,
-  port     : settings.port,
-  ssl      : settings.ssl
+    user: settings.user,
+    password: settings.password,
+    database: settings.database,
+    host: settings.hostname,
+    port: settings.port,
+    ssl: settings.ssl
 });
 
+const queryType = process.argv.slice(2)[0]
+let id = null;
+
 client.connect((err) => {
-  if (err) {
-    return console.error("Connection Error", err);
-  }
-  client.query("SELECT $1::int AS number", ["1"], (err, result) => {
     if (err) {
-      return console.error("error running query", err);
+        return console.error("Connection Error", err);
     }
-    console.log(result.rows[0].number); //output: 1
-    client.end();
-  });
+    console.log('Connected to pg server');
+
+    switch (queryType) {
+
+        case 'readFN':
+            first_name = process.argv.slice(2)[1];
+            client.query('SELECT * FROM famous_people WHERE first_name = $1', [first_name], (err, res) => {
+                if (err) throw err;
+                console.log(res.rows);
+                client.end();
+            });
+            break;
+
+        case 'readLN':
+            last_name = process.argv.slice(2)[1];
+            client.query('SELECT * FROM famous_people WHERE last_name = $1', [last_name], (err, res) => {
+                if (err) throw err;
+                console.log(res.rows);
+                client.end();
+            });
+            break;
+
+        default:
+            client.end();
+            break;
+    }
 });
