47b29b478aaf5ebac46a31989164e21db315bd54
S-Marks <marks.cherie.art@gmail.com>
2019-06-18 21:02:54 +0000

    data and input/output separate functions

---
+++ db.js
@@ -0,0 +1,15 @@
+const pg = require("pg");
+const settings = require("./settings"); // settings.json
+
+const client = new pg.Client({
+    user: settings.user,
+    password: settings.password,
+    database: settings.database,
+    host: settings.hostname,
+    port: settings.port,
+    ssl: settings.ssl
+});
+
+client.connect();
+
+module.exports = client;
--- lookup_people.js
+++ lookup_people.js
@@ -1,46 +1,30 @@
-const pg = require("pg");
-const settings = require("./settings"); // settings.json
-
-const client = new pg.Client({
-    user: settings.user,
-    password: settings.password,
-    database: settings.database,
-    host: settings.hostname,
-    port: settings.port,
-    ssl: settings.ssl
-});
+const db = require('./db');
 
 const queryType = process.argv.slice(2)[0]
-let id = null;
 
-client.connect((err) => {
-    if (err) {
-        return console.error("Connection Error", err);
-    }
-    console.log('Connected to pg server');
+switch (queryType) {
 
-    switch (queryType) {
+    case 'readFN':
+        console.log('Searching...');
+        first_name = process.argv.slice(2)[1];
+        db.query('SELECT * FROM famous_people WHERE first_name = $1', [first_name], (err, res) => {
+            if (err) throw err;
+            console.log(res.rows);
+            db.end();
+        });
+        break;
 
-        case 'readFN':
-            first_name = process.argv.slice(2)[1];
-            client.query('SELECT * FROM famous_people WHERE first_name = $1', [first_name], (err, res) => {
-                if (err) throw err;
-                console.log(res.rows);
-                client.end();
-            });
-            break;
+    case 'readLN':
+        console.log('Searching...');
+        last_name = process.argv.slice(2)[1];
+        db.query('SELECT * FROM famous_people WHERE last_name = $1', [last_name], (err, res) => {
+            if (err) throw err;
+            console.log(res.rows);
+            db.end();
+        });
+        break;
 
-        case 'readLN':
-            last_name = process.argv.slice(2)[1];
-            client.query('SELECT * FROM famous_people WHERE last_name = $1', [last_name], (err, res) => {
-                if (err) throw err;
-                console.log(res.rows);
-                client.end();
-            });
-            break;
-
-        default:
-            client.end();
-            break;
-    }
-});
+    default:
+        db.end();
+        break;
+}
